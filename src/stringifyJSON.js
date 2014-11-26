cb7d334b72c455dc3382bdcf2c57987d43b2f6cd
David Fedele <dave.fedele@gmail.com>
2014-11-26 09:53:44 -0800

    wip - starting work on recursing for array objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log("----------------------------------");
+  var stringified = "";
+
+  if (typeof obj === "number") {
+    console.log("number");
+    stringified += obj;
+  }
+  else if (typeof obj === "string") {
+    console.log("string");
+    stringified += '"' + obj +'"';
+  }
+  else if (typeof obj === "boolean") {
+    console.log("boolean");
+    stringified += Boolean(obj);
+  }
+  else if (obj === null) {
+    console.log("null");
+    stringified += "null";
+  }
+//returnecursive for all other objects
+  else if (Array.isArray(obj)) {
+    console.log("array");
+    stringified += "[";
+    for (var i=0; i<obj.length; i++) {
+      stringified += stringifyJSON(obj[i]);
+      if(obj.length < 1 || i<obj.length){
+        stringified += ",";
+      }
+    }
+    stringified += "]";
+  }
+  // else {
+  //   for (var key in obj) {
+  //     stringified += key;
+  //   }
+  // }
+
+  console.log(obj);
+
+  return stringified;
 };
