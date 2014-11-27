0c0caaf1ca76ab51975a94938e369f3f2ec7d958
David Fedele <dave.fedele@gmail.com>
2014-11-27 00:17:43 -0800

    removed the debugging console.log statements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,36 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  console.log("----------------------------------");
+  // console.log("----------------------------------");
   var stringified = "";
 
   if (typeof obj === "number") {
-    console.log("number");
+    // console.log("number");
     stringified += obj;
   }
   else if (typeof obj === "string") {
-    console.log("string");
+    // console.log("string");
     stringified += '"' + obj +'"';
   }
   else if (typeof obj === "boolean") {
-    console.log("boolean");
+    // console.log("boolean");
     stringified += Boolean(obj);
   }
   else if (obj === null) {
-    console.log("null");
+    // console.log("null");
     stringified += "null";
   }
   else if (obj === undefined) {
-    console.log("undefined");
+    // console.log("undefined");
     //do nothing
   }
   else if (typeof obj === "function") {
-    console.log("function");
+    // console.log("function");
     //do nothing
   }
 //return recursive for all other objects
   if (Array.isArray(obj)) {
-    console.log("array");
+    // console.log("array");
     stringified += "[";
     for (var i=0; i<obj.length; i++) {
       stringified += stringifyJSON(obj[i]);
@@ -45,7 +45,7 @@ var stringifyJSON = function(obj) {
     stringified += "]";
   }
   else if (Object.prototype.toString.call(obj) === "[object Object]") {
-    console.log("for strict objects")
+    // console.log("for strict objects")
     stringified += "{";
     if (Object.keys(obj).length === 0){
       ;
@@ -54,11 +54,11 @@ var stringifyJSON = function(obj) {
       var illegal = false;
       for (var key in obj) {
         if (obj[key] === undefined) {
-          console.log("skipping undefined values");
+          // console.log("skipping undefined values");
           illegal = true;
         }
         else if (typeof obj[key] === "function") {
-          console.log("skipping functions");
+          // console.log("skipping functions");
           illegal = true;
         }
         else{
@@ -72,8 +72,8 @@ var stringifyJSON = function(obj) {
     stringified += "}";
   }
 
-  console.log("----------------------------------");
-  console.log(obj);
-  console.log("JSON_stringified = " + stringified);
+  // console.log("----------------------------------");
+  // console.log(obj);
+  // console.log("JSON_stringified = " + stringified);
   return stringified;
 };
