6a3d655e191f3ef2c686b29e6d292ba0d6fcd7e7
David Fedele <dave.fedele@gmail.com>
2014-11-28 22:35:10 -0800

    working on children of document.body - needs recursion to all sub children now

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,27 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+  // If life was easy, we could just do things the easy way:
+  // var getElementsByClassName = function (className) {
+  //   return document.getElementsByClassName(className);
+  // };
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
-};
+  // But instead we're going to implement it from scratch:
+  var getElementsByClassName = function(className){
+    var result = []
+
+    var documentBody = document.body;
+
+    var nodeChildren = documentBody.childNodes;
+
+    for (var i=0; i<nodeChildren.length; i++) {
+      var nodeClasses = nodeChildren[i].classList;
+      if (nodeClasses && nodeClasses.length !== 0) {
+        console.log(nodeChildren[i] + " has these classes: " + nodeClasses);
+        for (var j=0; j<nodeClasses.length; j++) {
+          if (nodeClasses[j] === className) {
+            result.push(nodeChildren[i]);
+          }
+        }
+      }
+    }
+
+    return result;
+  };
