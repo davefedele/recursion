9f838dcf6f7722aaee06b20dd9d937e83fa663a2
David Fedele <dave.fedele@gmail.com>
2014-11-28 23:09:59 -0800

    test passing for getElementsByClassName - needs rework with underscore

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,23 +5,27 @@
 
   // But instead we're going to implement it from scratch:
   var getElementsByClassName = function(className){
-    var result = []
+    var result = [];
 
-    var documentBody = document.body;
+    var documentBody = document;
 
-    var nodeChildren = documentBody.childNodes;
-
-    for (var i=0; i<nodeChildren.length; i++) {
-      var nodeClasses = nodeChildren[i].classList;
-      if (nodeClasses && nodeClasses.length !== 0) {
-        console.log(nodeChildren[i] + " has these classes: " + nodeClasses);
-        for (var j=0; j<nodeClasses.length; j++) {
-          if (nodeClasses[j] === className) {
-            result.push(nodeChildren[i]);
+    function childRecursion(startingPoint) {
+      var nodeChildren = startingPoint.childNodes;
+      for (var i=0; i<nodeChildren.length; i++) {
+        var nodeClasses = nodeChildren[i].classList;
+        if (nodeClasses && nodeClasses.length !== 0) {
+          console.log(nodeChildren[i] + " has these classes: " + nodeClasses);
+          for (var j=0; j<nodeClasses.length; j++) {
+            if (nodeClasses[j] === className) {
+              result.push(nodeChildren[i]);
+            }
           }
         }
+        childRecursion(nodeChildren[i]);
       }
     }
+
+    childRecursion(documentBody);
 
     return result;
   };
